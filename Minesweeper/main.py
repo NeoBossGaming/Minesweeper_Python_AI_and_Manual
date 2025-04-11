ba3c784c87ebc25f62ee3fa17f4d5b62b449b83b
NeoBossGaming <neomaxwell27@gmail.com>
2025-04-11 08:29:37 +0700

    Update main.py

--- Minesweeper/main.py
+++ Minesweeper/main.py
@@ -2,6 +2,8 @@ import AIPlayer
 import ManualPlayer
 
 difficulty = int(input("Select Difficulty(1, 2, or 3): "))
+while difficulty not in [1, 2, 3]:
+    difficulty = int(input("Select Difficulty(1, 2, or 3): "))
 mode = input("Select Mode(\"Player or AI\")")
 if mode == "Player":
     ManualPlayer.runGamePlayer(difficulty, "Neo")
@@ -10,7 +12,8 @@ if mode == "AI":
     times = int(input('How many times : '))
     for t in range(times):
          if AIPlayer.runGame(difficulty):
-             score += 1
+            score += 1
+            print(f"Win    Round: {t}")
          else:
-            print("Boom")
+            print(f"Lost    Round: {t}")
     print(f'Your AI got {score}/{times}')
